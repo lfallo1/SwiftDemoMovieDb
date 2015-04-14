76045d8f96cbde5bbee0194486de8c1c29fd8bc2
Lance Fallon <lancefallon@Lances-MacBook-Pro.local>
2015-04-14 19:50:48 -0400

    Initial Commit

---
+++ IMDB Search/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IMDB Search
+//
+//  Created by Lance Fallon on 4/14/15.
+//  Copyright (c) 2015 Lance Fallon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
